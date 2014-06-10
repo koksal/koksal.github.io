25700f12d9c3308d8aba1ec7dbeafe830e345183
Ali Sinan Köksal <alisinan@gmail.com>
2014-06-09 19:09:01 -0700

    watch and clean tasks in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,31 @@ module.exports = function(grunt) {
           "assets/css/main.css": "_less/main.less"
         }
       }
+    },
+    watch: {
+      less: {
+        files: [
+          "_less/*.less"
+        ],
+        tasks: ['less']
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/main.css'
+      ]
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.registerTask('default', ['less']);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [
+    'clean',
+    'less'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ])
 };
